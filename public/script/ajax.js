c4d089182e5cb00fff456a72300c3803f527f7d1
manu <kunmanu@live.fr>
2022-11-02 22:27:17 +0100

    2/11 2226

--- public/script/ajax.js
+++ public/script/ajax.js
@@ -1,46 +1,67 @@
 const API_KEY= "be9480de1d60eae1fa03ff863915a3ff"
+const API_IMG= "https://image.tmdb.org/t/p/w185"
 
-let movieCard
-let moviePoster
-let movieTitle
+// const movieCard = document.querySelector('.movieCard')
+const cardContainer = document.querySelector('.cardContainer')
 
 
 
 const getData = async(queryString) => {
     const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${queryString}&page=1&include_adult=false`)
     const result = await response.json()
-
-    // console.log(result)
     return result
 }
 
 
-function movieCardBuild(result){
-    for ( let i = 0; i < result.results.length ; i++){
-        let posterSource = result.results[i].poster_path
-        let titleSource = result.results[i].title
+Object.prototype.setAttr = function (nom, valeur) {
+    this.setAttribute(nom, valeur)
+    return this
+}
 
-        
-
-        movieCard.innerHTML += `<h4 class='movieTitle'>${titleSource}</h4>`
-        posterSource != null ? movieCard.innerHTML += `<img src="https://image.tmdb.org/t/p/w185/${posterSource}" class="moviePoster">` : movieCard.innerHTML += "<p>Pas d'image</p>"
-    }
+Object.prototype.appendC = function (child) {
+    this.appendChild(child)
+    return this
 }
 
 
 
+
+function movieCardBuild(data){
+
+    data.results.forEach(movie => {
+        let poster_path = movie.poster_path
+        let titleSource = movie.title
+        let posterSource = API_IMG+poster_path
+        let card = document.createElement('div')
+        let title = document.createElement('h4')
+        let poster = document.createElement('img')
+        
+        card.appendC(title)
+            .appendC(poster)
+
+
+        cardContainer.appendC(card)
+        
+        
+        card.setAttribute('class','cardContainer-movieCard movieCard')
+        title.setAttribute('class','cardContainer-movieCard-movieTitle' )
+        title.innerHTML= titleSource
+
+        poster.setAttr('class', 'cardContainer-movieCard-moviePoster')
+              .setAttr('src', posterSource)
+
+    });
+}
+
+
 document.querySelector('.submitBtn').addEventListener("click", async (e)=>{
     e.preventDefault()
-    movieCard = document.querySelector('.movieCard')
-    moviePoster = document.querySelectorAll('.moviePoster')
-    movieTitle = document.querySelectorAll('.movieTitle')
+    console.log(e);
     let form = document.querySelector('.movieSearchForm')
-    movieCard.innerHTML= null
     let queryString = form.elements['searchInput'].value
-
-    const result = await getData(queryString)
-    // console.log(result.results)
-    result.results.length !== 0 ? movieCardBuild(result) : movieCard.innerHTML = "Aucun résultat"
+    const data = await getData(queryString)
+    cardContainer.innerHTML= null
+    data.results.length !== 0 ? movieCardBuild(data) : cardContainer.innerHTML = "Aucun résultat"
 })
 
 
